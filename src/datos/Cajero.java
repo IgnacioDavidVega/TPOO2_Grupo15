190da4e0d8ccba0b68753b76a487c84ed512cfd0
Ignacio David Vega <ignaciodavidvega@outlook.com>
2026-09-01 19:16:11 -0300

    Agregar clase Cajero que extiende de Personal con atributos y métodos específicos

---
+++ src/datos/Cajero.java
@@ -0,0 +1,30 @@
+package datos;
+
+import java.time.LocalDate;
+
+public class Cajero extends Personal {
+    private String turnoTrabajo;
+
+    public Cajero() {
+    }
+
+    public Cajero(String nombre, String apellido, int dni, LocalDate fechaNacimiento, LocalDate fechaIngreso,
+            double sueldoBase, int idPersonal, String turnoTrabajo) {
+        super(nombre, apellido, dni, fechaNacimiento, fechaIngreso, sueldoBase);
+        this.idPersonal = idPersonal;
+        this.turnoTrabajo = turnoTrabajo;
+    }
+
+    public String getTurnoTrabajo() {
+        return turnoTrabajo;
+    }
+
+    public void setTurnoTrabajo(String turnoTrabajo) {
+        this.turnoTrabajo = turnoTrabajo;
+    }
+
+    @Override
+    public String toString() {
+        return "Cajero [" + super.toString() + ", turnoTrabajo=" + turnoTrabajo + "]";
+    }
+}
