bdf64dcb19667341ed4819fe34952641a17460e2
Ignacio David Vega <ignaciodavidvega@outlook.com>
2026-09-01 19:07:55 -0300

    Agregar clase Cocinero que extiende de Personal con atributos y métodos específicos

---
+++ src/datos/Cocinero.java
@@ -0,0 +1,41 @@
+package datos;
+
+import java.time.LocalDate;
+
+public class Cocinero extends Personal {
+    private String especialidadCulinaria;
+    private double plusCategoria;
+
+    public Cocinero() {
+    }
+
+    public Cocinero(String nombre, String apellido, int dni, LocalDate fechaNacimiento, LocalDate fechaIngreso,
+            double sueldoBase, int idPersonal, String especialidadCulinaria, double plusCategoria) {
+        super(nombre, apellido, dni, fechaNacimiento, fechaIngreso, sueldoBase);
+        this.idPersonal = idPersonal;
+        this.especialidadCulinaria = especialidadCulinaria;
+        this.plusCategoria = plusCategoria;
+    }
+
+    public String getEspecialidadCulinaria() {
+        return especialidadCulinaria;
+    }
+
+    public void setEspecialidadCulinaria(String especialidadCulinaria) {
+        this.especialidadCulinaria = especialidadCulinaria;
+    }
+
+    public double getPlusCategoria() {
+        return plusCategoria;
+    }
+
+    public void setPlusCategoria(double plusCategoria) {
+        this.plusCategoria = plusCategoria;
+    }
+
+    @Override
+    public String toString() {
+        return "Cocinero [" + super.toString() + ", especialidadCulinaria=" + especialidadCulinaria
+                + ", plusCategoria=" + plusCategoria + "]";
+    }
+}
